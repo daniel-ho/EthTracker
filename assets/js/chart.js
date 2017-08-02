d5563caf9f0688fc6bfdb292a1340dc644a6e649
Daniel <danielho@berkeley.edu>
2017-08-02 16:25:15 -0700

    fixed setinterval call

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,18 +1,19 @@
+var url = "https://min-api.cryptocompare.com/data/histominute?fsym=ETH&tsym=USD&limit=1440&e=CCCAGG"
+
 function plot(data) {
 	console.log(data);
 }
 
-function updatePlot(url, plot_fn) {
+function updatePlot() {
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function () {
 		if (this.readyState === 4 && this.status === 200) {
 			var data = JSON.parse(this.responseText);
-			plot_fn(data);
+			plot(data);
 		}
 	}
 	xmlhttp.open("GET", url, true);
 	xmlhttp.send();
 }
 
-var url = "https://min-api.cryptocompare.com/data/histominute?fsym=ETH&tsym=USD&limit=1440&e=CCCAGG"
-setInterval(updatePlot(url, plot), 30000);
+setInterval(updatePlot, 30000);
