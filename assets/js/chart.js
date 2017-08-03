6e1e001605ef34fa54508165fc76b28e56a35f5a
Daniel <danielho@berkeley.edu>
2017-08-02 17:27:13 -0700

    check data parsing

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,7 +1,15 @@
 var url = "https://min-api.cryptocompare.com/data/histominute?fsym=ETH&tsym=USD&limit=1440&e=CCCAGG"
 
-var plot = function(data) {
-	console.log(data);
+var plot = function(input) {
+	var data = input["Data"];
+	var timeFrom = input["timeFrom"];
+	var timeTo = input["timeTo"];
+	var timeToPrice = {};
+	for (var i = 0; i < data.length; i++) {
+		time = data[i]["time"]
+		timeToPrice[time] = data[i]["close"]
+	}
+	console.log(timeToPrice);
 }
 
 var updatePlot = function() {
@@ -16,4 +24,4 @@ var updatePlot = function() {
 	xhr.send();
 }
 
-setInterval(updatePlot, 30000);
+setInterval(updatePlot, 60000);
