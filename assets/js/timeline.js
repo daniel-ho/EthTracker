12bae9d208c34c07e2e34951e1082f45985fd2d9
Daniel Ho <danielho@berkeley.edu>
2017-08-11 00:56:47 -0700

    removed timeline code

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -2,16 +2,3 @@
 
 url = "https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&e=CCCAGG&allData=true";
 timelineChart = undefined;
-
-var createChart = function(data) {
-	var width = $("section#timeline").select().width();
-	var height = $("section#timeline").select().height();
-	var svg = dimple.newSvg("section#timeline", "100%", "40%");
-	svg.attr("viewBox", "0 0 " + width + " " + height);
-
-	timelineChart = new dimple.chart(svg, data);
-	var x = timelineChart.addTimeAxis("x", "time", "%Y %b %d %H:%M", "%b %d %Y");
-	var y = ethChart.addMeasureAxis("y", "close");
-	y.tickFormat = ",.2f";
-	var series = ethChart.addSeries(null, dimple.plot.area);
-}
