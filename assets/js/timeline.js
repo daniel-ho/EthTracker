2e0b9e3db4e4e6a6a41a465c5b5b5b98e7d22444
Daniel <danielho@berkeley.edu>
2017-08-12 13:43:19 -0700

    added missing time parameter

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -4,7 +4,7 @@ timelineChart = {value 	   : undefined,
 				 url 	   : "https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&e=CCCAGG&allData=true",
 				 curr_zoom : "All Time"};
 
-timelineData = [{"time" : , "close" : "218.12", "Event" : "Bitcoin Hard Fork"}]
+timelineData = [{"time" : "2017 Aug 1 00:00", "close" : "218.12", "Event" : "Bitcoin Hard Fork"}]
 
 updatePlot(timelineChart, "section#timeline", "All Time");
 eventSeries = timelineChart.value.addSeries(null, dimple.plot.bubble);
