15eb85a0f74c7acd1c4173fa34c8a7ce40ba9adf
Daniel <danielho@berkeley.edu>
2017-08-12 14:53:43 -0700

    events plotted on color axis z

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -8,7 +8,10 @@ timelineData = [{"time" : "2017 Jul 18 00:00", "close" : "194.41", "Event" : "EE
 				{"time" : "2017 Aug 1 00:00", "close" : "218.12", "Event" : "Bitcoin Hard Fork"}]
 
 var overlayEvents = function() {
-	eventSeries = timelineChart.value.addSeries("Event", dimple.plot.bubble);
+	x = timelineChart.value.axes[0];
+	y = timelineChart.value.axes[1];
+	z = timelineChart.addColorAxis("Event", "green");
+	eventSeries = timelineChart.value.addSeries(null, dimple.plot.bubble, [x, y, z]);
 	eventSeries.data = timelineData;
 }
 
