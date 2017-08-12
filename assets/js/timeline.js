74d869c383bfb1674a84c5f288769e67019da245
Daniel <danielho@berkeley.edu>
2017-08-12 14:14:13 -0700

    added Events to category field of bubble series

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -7,7 +7,7 @@ timelineChart = {value 	   : undefined,
 timelineData = [{"time" : "2017 Aug 1 00:00", "close" : "218.12", "Event" : "Bitcoin Hard Fork"}]
 
 var overlayEvents = function() {
-	eventSeries = timelineChart.value.addSeries(null, dimple.plot.bubble);
+	eventSeries = timelineChart.value.addSeries("Event", dimple.plot.bubble);
 	eventSeries.data = timelineData;
 }
 
