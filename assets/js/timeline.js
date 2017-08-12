26b44c221df94c69d8ee58e8f5ab50d793dfbe67
Daniel <danielho@berkeley.edu>
2017-08-12 13:41:20 -0700

    testing timeline scatter plot overlay

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -4,35 +4,8 @@ timelineChart = {value 	   : undefined,
 				 url 	   : "https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&e=CCCAGG&allData=true",
 				 curr_zoom : "All Time"};
 
-/* timelineData = [{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : }] */
+timelineData = [{"time" : , "close" : "218.12", "Event" : "Bitcoin Hard Fork"}]
 
 updatePlot(timelineChart, "section#timeline", "All Time");
+eventSeries = timelineChart.value.addSeries(null, dimple.plot.bubble);
+eventSeries.data = timelineData;
