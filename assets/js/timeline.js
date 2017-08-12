d085d7152c511dc3c27170c64b931c8526d0ca7b
Daniel <danielho@berkeley.edu>
2017-08-12 13:24:12 -0700

    commented out timelineData object

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -4,7 +4,7 @@ timelineChart = {value 	   : undefined,
 				 url 	   : "https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&e=CCCAGG&allData=true",
 				 curr_zoom : "All Time"};
 
-timelineData = [{"time" : , "close" : , "Event" : },
+/* timelineData = [{"time" : , "close" : , "Event" : },
 				{"time" : , "close" : , "Event" : },
 				{"time" : , "close" : , "Event" : },
 				{"time" : , "close" : , "Event" : },
@@ -33,6 +33,6 @@ timelineData = [{"time" : , "close" : , "Event" : },
 				{"time" : , "close" : , "Event" : },
 				{"time" : , "close" : , "Event" : },
 				{"time" : , "close" : , "Event" : },
-				{"time" : , "close" : , "Event" : }]
+				{"time" : , "close" : , "Event" : }] */
 
 updatePlot(timelineChart, "section#timeline", "All Time");
